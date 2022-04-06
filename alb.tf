2d100171f1305f3ae6b8d241aadb14dec5dbd923
willisxiong <xiongwei.fly@gmail.com>
2022-04-06 21:36:04 +0800

    change listener port and traffic port to 5000

--- alb.tf
+++ alb.tf
@@ -16,10 +16,10 @@ resource "aws_lb" "alb" {
 
 }
 
-#Create target group, default target type is instance
+# Create target group, default target type is instance
 resource "aws_lb_target_group" "alb_tg" {
   name     = "reliable-alb-tg"
-  port     = 80
+  port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
@@ -34,8 +34,8 @@ resource "aws_lb_target_group" "alb_tg" {
     # between health checks of an individual target
     interval = 30
     # Alb send http health check request to index.html
-    path = "/healthcheck.html"
-    port = "traffic-port"
+    path = "/"
+    port = 5000
   }
 
   tags = {
@@ -49,10 +49,10 @@ resource "aws_lb_target_group_attachment" "poc" {
   target_id = aws_instance.vm1.id
 }*/
 
-# Create port 80 listener
+# Create port 5000 listener
 resource "aws_lb_listener" "alb_listner" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = 80
+  port              = 5000
   protocol          = "HTTP"
 
   default_action {
