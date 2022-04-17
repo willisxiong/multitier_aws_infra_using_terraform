16abc9783a149245a77d6ba6a81ccf89ded2fdc0
willisxiong <xiongwei.fly@gmail.com>
2022-04-17 13:01:07 +0800

    use count argument to create repeatable resources

--- nat.tf
+++ nat.tf
@@ -2,8 +2,10 @@
 # and install web server from internet so that
 # alb's http health check available
 
-# Eip1 will attach to nat1 in public subnet1
-resource "aws_eip" "nat_eip1" {
+# Create 3 elastic ip
+resource "aws_eip" "nat_eip" {
+  count = length(var.public-subnets)
+
   vpc = true
 
   depends_on = [
@@ -11,38 +13,10 @@ resource "aws_eip" "nat_eip1" {
   ]
 }
 
-# Eip2 will attach to nat2 in public subnet2
-resource "aws_eip" "nat_eip2" {
-  vpc = true
+# Create 3 nat gateway in each public subnet
+resource "aws_nat_gateway" "nat" {
+  count = length(var.public-subnets)
 
-  depends_on = [
-    aws_internet_gateway.igw
-  ]
-}
-
-# Eip3 will attach to nat3 in public subnet3
-resource "aws_eip" "nat_eip3" {
-  vpc = true
-
-  depends_on = [
-    aws_internet_gateway.igw
-  ]
-}
-
-# Create nat in public subnet1
-resource "aws_nat_gateway" "nat1" {
-  allocation_id = aws_eip.nat_eip1.id
-  subnet_id     = aws_subnet.public1.id
-}
-
-# Create nat in public subnet2
-resource "aws_nat_gateway" "nat2" {
-  allocation_id = aws_eip.nat_eip2.id
-  subnet_id     = aws_subnet.public2.id
-}
-
-# Create nat in public subnet3
-resource "aws_nat_gateway" "nat3" {
-  allocation_id = aws_eip.nat_eip3.id
-  subnet_id     = aws_subnet.public3.id
+  allocation_id = element(aws_eip.nat_eip[*].id, count.index)
+  subnet_id     = element(aws_subnet.public[*].id, count.index)
 }
