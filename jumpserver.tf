6d3781d58f178a98f054d027e94afc6f9f7ee41f
willisxiong <xiongwei.fly@gmail.com>
2022-04-17 13:09:32 +0800

    modify subnet argument as the aws_subnet resource changed

--- jumpserver.tf
+++ jumpserver.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "jumpserver" {
   ami                    = data.aws_ssm_parameter.amz_ami.value
   instance_type          = var.ec2-type
-  subnet_id              = aws_subnet.public1.id
+  subnet_id              = element(aws_subnet.public[*].id, 0)
   key_name               = "myvpckey"
   vpc_security_group_ids = [aws_security_group.alb_sg.id]
   tags = {
